64b49095e3a47912444a0b4030f39cf36bfb0f0f
Deepak Pandey <pandeydeepak821@gmail.com>
2022-07-22 16:24:31 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const PORT = 3000 || process.env.PORT;
+const path = require('path');
+const ejs = require('ejs');
+const expressLayout = require('express-ejs-layouts')
+
+app.get('/', (req,res) => {
+    res.render('home')
+})
+
+// Set Template Engine
+app.use(expressLayout);
+
+app.set('views', path.join(__dirname, '/resources/views'));
+app.set('view engine', 'ejs');
+
+app.listen(PORT, () =>{
+    console.log(`connected at port ${PORT}`)
+})
