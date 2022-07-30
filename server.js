5e290183faed06877c08c2293452ac39b3d1c59b
Deepak Pandey <pandeydeepak821@gmail.com>
2022-07-30 19:11:30 +0530

    almost completed the UI Part

--- server.js
+++ server.js
@@ -7,15 +7,27 @@ const expressLayout = require('express-ejs-layouts')
 
 app.use(express.static('public'));
 
-app.get('/', (req,res) => {
-    res.render('home');
-})
-
 // Set Template Engine
 app.use(expressLayout);
 
 app.set('views', path.join(__dirname, '/resources/views'));
 app.set('view engine', 'ejs');
+
+app.get('/', (req,res) => {
+    res.render('home');
+})
+
+app.get('/cart', (req,res) => {
+    res.render('customers/cart');
+})
+
+app.get('/login', (req, res) => {
+    res.render('auth/login');
+})
+
+app.get('/register', (req, res) => {
+    res.render('auth/register');
+})
 
 app.listen(PORT, () =>{
     console.log(`connected at port ${PORT}`)
